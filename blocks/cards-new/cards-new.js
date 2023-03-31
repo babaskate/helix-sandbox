9c11ff18a6b1c149c94372823f16674d17eb71e6
Andrew Babaian <andrew.babaian@herodigital.com>
2023-03-31 09:39:02 -0400

    chore(cards-new): rename component

--- blocks/cards-new/cards-new.js
+++ blocks/cards-new/cards-new.js
@@ -7,8 +7,8 @@ export default function decorate(block) {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cardsnew-card-image';
-      else div.className = 'cardsnew-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-new-card-image';
+      else div.className = 'cards-new-card-body';
     });
     ul.append(li);
   });
